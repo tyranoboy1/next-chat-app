3feda58f5851350aaaf65db04251b18d40e7e197
JiHoon <gkwlgnd@naver.com>
2024-04-15 23:41:00 +0900

    [UPDATE] next auth 설정

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,9 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
   return (
     <ul
       className={`text-md justify-center flex w-full gap-4 ${
@@ -14,12 +16,15 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
       <li className={`py-2 text-center border-b-4 cursor-pointer`}>
         <Link href={"/user"}>User</Link>
       </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Signup</button>
-      </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Signin</button>
-      </li>
+      {session?.user ? (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signOut()}>Signout</button>
+        </li>
+      ) : (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signIn()}>Signin</button>
+        </li>
+      )}
     </ul>
   );
 };
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default NextAuth(authOptions);
